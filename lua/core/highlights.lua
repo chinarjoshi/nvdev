4b0940f677f366ba2700b38abfdc85e5ea70c19c
Chinar Joshi <chinarjoshi7@gmail.com>
2022-03-23 12:33:21 -0400

    Add highlights file

---
+++ lua/core/highlights.lua
@@ -0,0 +1,11 @@
+for foreground, color in pairs({
+    DiagnosticHint = 'purple',
+    DiagnosticError = 'red',
+    DiagnosticWarn = 'yellow',
+    DiagnosticInformation = 'green',
+    DiagnosticInfo = 'blue',
+    PmenuSel
+}) do
+    vim.cmd
+    
+end
