2a1908e7b2250ebca28fe6b4eb0de5dd3cf726ac
Chinar Joshi <chinarjoshi7@gmail.com>
2023-05-16 01:59:10 -0400

    Make lsp diagnostic change number line color instead of sign

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -54,10 +54,9 @@ for _, server in ipairs(vim.g.language_servers) do
   lspconfig[server].setup { on_attach = M.on_attach }
 end
 
-local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-for type, icon in pairs(signs) do
+for _, type in ipairs { "Error", "Warn", "Hint", "Info" } do
   local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  vim.fn.sign_define(hl, { text = "", texthl = hl, numhl = hl })
 end
 
 vim.diagnostic.config {
