59301f53599eedd5711d74ce4d0b219e78aba027
Chinar Joshi <chinarjoshi7@gmail.com>
2023-05-19 19:58:11 -0400

    replace /home/c/ with ~

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -67,7 +67,7 @@ require('lazy').setup({
       {
         'mfussenegger/nvim-dap-python',
         config = function()
-          require('dap-python').setup('/home/c/.local/share/nvim/mason/packages/debugpy/venv/bin/python')
+          require('dap-python').setup('~/.local/share/nvim/mason/packages/debugpy/venv/bin/python')
         end,
       },
 
